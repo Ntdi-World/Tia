e96854a529a7bedaa38e5e5e09292c5d5cb4596b
Ntdi <79174456+n-tdi@users.noreply.github.com>
2022-12-18 11:29:25 -0500

    Update to run on port 80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-//joining path of directory 
+// Joining path of directory 
 const directoryPath = path.join(__dirname, 'pic');
 const express = require('express')
 const app = express()
@@ -8,9 +8,9 @@ const app = express()
 let pic;
 const url = "https://raw.githubusercontent.com/n-tdi/Tia/main/pic/";
 
-//passsing directoryPath and callback function
+// Passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
+    // Handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
@@ -18,24 +18,39 @@ fs.readdir(directoryPath, function (err, files) {
     pic = files;
 });
 
+// Proudly stolen from https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
 // Create express route at /tia
 app.get('/tia', (req, res) => {
     res.setHeader('Content-Type', 'application/json'); // Respond with json
     num = Math.floor(Math.random()*pic.length);
-    geture = url + pic[num]; // get random tia pic out of folder
-    res.end(JSON.stringify({ id: num, picture: geture }, null, 3)); // return json
+    geture = url + pic[num]; // Get random tia pic out of folder
+    res.end(JSON.stringify({ id: num, picture: geture }, null, 3)); // Return json
 })
 
 // Create express route at /tia/:id to get sdpecific tia pic
 app.get('/tia/:id', (req, res) => {
     res.setHeader('Content-Type', 'application/json'); // Respond with json
-    num = parseInt(req.params.id);
-    if (num > pic.length - 1) {
-        res.sendStatus(404);
-        return;
+    
+    if (!isNumeric(req.params.id)) { // Check if the suppyled id is a numeric value
+        res.sendStatus(400); // Send a bad request error code
+        return; // Stop the request
     }
-    geture = url + pic[num]; // get random tia pic out of folder
-    res.end(JSON.stringify({ id: num, picture: geture }, null, 3)); // return json
+
+    num = parseInt(req.params.id); // Parse the reqest as an int. Probably will cause issues with strings :)
+    
+    if (num > pic.length - 1) { // If the supplyed int is not a valid tia pic, return 404
+        res.sendStatus(404); // Send a not found error code
+        return; // Stop the request
+    }
+
+    geture = url + pic[num]; // Get random tia pic out of folder
+    res.end(JSON.stringify({ id: num, picture: geture }, null, 3)); // Return json
 })
 
-app.listen(3000);
+app.listen(80);
