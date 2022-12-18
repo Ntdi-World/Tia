49a54947ac50c762ab135f336b8a9c361e93e0a5
Ntdi <79174456+n-tdi@users.noreply.github.com>
2022-12-18 11:07:45 -0500

    update to have /tia route

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const path = require('path');
 const fs = require('fs');
 //joining path of directory 
 const directoryPath = path.join(__dirname, 'pic');
+const express = require('express')
+const app = express()
 
 let pic;
-let done = false;
+const url = "https://raw.githubusercontent.com/n-tdi/Tia/main/pic/";
 
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
@@ -12,8 +14,16 @@ fs.readdir(directoryPath, function (err, files) {
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
-    //listing all files using forEach
+
     pic = files;
-    console.log(pic);
 });
 
+// Create express route for root at /tia
+app.get('/tia', (req, res) => {
+    res.setHeader('Content-Type', 'application/json'); // Respond with json
+    num = Math.floor(Math.random()*pic.length);
+    geture = url + pic[num]; // get random tia pic out of folder
+    res.end(JSON.stringify({ id: num, picture: geture }, null, 3)); // return json
+})
+
+app.listen(3000);
